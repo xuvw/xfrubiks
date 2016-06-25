466b77be5b93b3a16e5bbad5d1fe623d12ff16bd
EverStar <1347508335@qq.com>
2016-06-25 17:19:06 +0800

    input

--- xfrubiks/xfrubiks/MenuPanel/MenuViewController.m
+++ xfrubiks/xfrubiks/MenuPanel/MenuViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "MenuViewController.h"
+#import "iflyMSC/IFlyMSC.h"
+#import "IATConfig.h"
+#import "ISRDataHelper.h"
 
-@interface MenuViewController ()
-
+@interface MenuViewController ()<IFlySpeechRecognizerDelegate>
+@property (nonatomic, strong) IFlySpeechRecognizer *iFlySpeechRecognizer;
+@property (nonatomic, copy) NSString *inputStr;
 @end
 
 @implementation MenuViewController
@@ -22,6 +26,73 @@
     
     
     
+}
+
+- (void)initRecognizer
+{
+    //单例模式，无UI的实例
+    if (_iFlySpeechRecognizer == nil) {
+        _iFlySpeechRecognizer = [IFlySpeechRecognizer sharedInstance];
+        
+        [_iFlySpeechRecognizer setParameter:@"" forKey:[IFlySpeechConstant PARAMS]];
+        
+        //设置听写模式
+        [_iFlySpeechRecognizer setParameter:@"iat" forKey:[IFlySpeechConstant IFLY_DOMAIN]];
+    }
+    _iFlySpeechRecognizer.delegate = self;
+    
+    if (_iFlySpeechRecognizer != nil) {
+        IATConfig *instance = [IATConfig sharedInstance];
+        
+        //设置最长录音时间
+        [_iFlySpeechRecognizer setParameter:instance.speechTimeout forKey:[IFlySpeechConstant SPEECH_TIMEOUT]];
+        //设置后端点
+        [_iFlySpeechRecognizer setParameter:instance.vadEos forKey:[IFlySpeechConstant VAD_EOS]];
+        //设置前端点
+        [_iFlySpeechRecognizer setParameter:instance.vadBos forKey:[IFlySpeechConstant VAD_BOS]];
+        //网络等待时间
+        [_iFlySpeechRecognizer setParameter:@"20000" forKey:[IFlySpeechConstant NET_TIMEOUT]];
+        
+        //设置采样率，推荐使用16K
+        [_iFlySpeechRecognizer setParameter:instance.sampleRate forKey:[IFlySpeechConstant SAMPLE_RATE]];
+        
+        if ([instance.language isEqualToString:[IATConfig chinese]]) {
+            //设置语言
+            [_iFlySpeechRecognizer setParameter:instance.language forKey:[IFlySpeechConstant LANGUAGE]];
+            //设置方言
+            [_iFlySpeechRecognizer setParameter:instance.accent forKey:[IFlySpeechConstant ACCENT]];
+        }else if ([instance.language isEqualToString:[IATConfig english]]) {
+            [_iFlySpeechRecognizer setParameter:instance.language forKey:[IFlySpeechConstant LANGUAGE]];
+        }
+        //设置是否返回标点符号
+        [_iFlySpeechRecognizer setParameter:instance.dot forKey:[IFlySpeechConstant ASR_PTT]];
+        
+    }
+    
+}
+
+#pragma mark IFlySpeechRecognizerDelegate
+- (void) onResults:(NSArray *) results isLast:(BOOL)isLast
+{
+    
+    NSMutableString *resultString = [[NSMutableString alloc] init];
+    NSDictionary *dic = results[0];
+    for (NSString *key in dic) {
+        [resultString appendFormat:@"%@",key];
+    }
+//    _inputStr =[NSString stringWithFormat:@"%@%@", _label.text,resultString];
+    NSString * resultFromJson =  [ISRDataHelper stringFromJson:resultString];
+//    _label.text = [NSString stringWithFormat:@"%@%@", _label.text,resultFromJson];
+    
+    if (isLast){
+        NSLog(@"听写结果(json)：%@测试",  self.inputStr);
+    }
+    NSLog(@"_result=%@",_inputStr);
+    NSLog(@"resultFromJson=%@",resultFromJson);
+//    NSLog(@"isLast=%d,_textView.text=%@",isLast,_label.text);
+}
+- (void) onError:(IFlySpeechError *) errorCode{
+    NSLog(@"%@", errorCode);
 }
 
 - (void)didReceiveMemoryWarning {
