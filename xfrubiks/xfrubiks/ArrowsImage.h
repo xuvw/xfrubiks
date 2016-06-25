f46654ea9a0286bd87cf163e531d7fbbb988dbf2
EverStar <1347508335@qq.com>
2016-06-26 04:15:18 +0800

    x

---
+++ xfrubiks/xfrubiks/ArrowsImage.h
@@ -0,0 +1,16 @@
+//
+//  arrowsImage.h
+//  xfrubiks
+//
+//  Created by L on 16/6/26.
+//  Copyright © 2016年 xfteam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ArrowsImage : NSObject
++ (UIImageView *)getRightImage;
++ (UIImageView *)getLeftImage;
++ (UIImageView *)getUpImage;
++ (UIImageView *)getDownImage;
+@end
